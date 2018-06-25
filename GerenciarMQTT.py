bb9e2d4e3c425ead4745a3ca61ff56d92c2804ad
ThiagoASilva <thiago.afonso2010@hotmail.com>
2018-06-24 22:13:32 -0300

    Adicionado a verificação de comandos via MQTT e o repasse desses comandos para o ESP8266

--- GerenciarMQTT.py
+++ GerenciarMQTT.py
@@ -11,17 +11,17 @@ class GerenciarMQTT:
         self.PortaBroker = 1883
         self.KeepAliveBroker = 60
         self.TopicoSubscribe = "TCCMEDICAO"
+        self.TopicoComando="TCCCOMANDO"
         self.Marca = "[EnergyControl]"
         self.Banco = DAO.DAO()
         #Fim Definicoes------
         self.iniciarMqtt()
 
-
-
     def on_connect(self, client, userdata, flags, rc):
         print(self.Marca + "##STATUS## Conectado ao Broker. Resultado de conexao: " + str(rc))
         # faz subscribe automatico no topico
         self.cliente.subscribe(self.TopicoSubscribe)
+        self.cliente.subscribe(self.TopicoComando)
 
     # callback - mensagem recebida do broker
     def on_message(self, client, userdata, msg):
@@ -43,18 +43,39 @@ class GerenciarMQTT:
     def principal(self):
         self.cliente.loop_forever()
 
+    def enviarMensagem(self, mensagem):
+        self.cliente.publish(self.TopicoComando, mensagem)
+        print(self.Marca + "##MENSAGEM ENVIADA## " + mensagem)
+
+
     def definirMensagensR(self, MensagemRecebida):
         linhaN = TString.TString()
         assunto = linhaN.obterassunto(MensagemRecebida)
 
-        #SEPARANDO OS ASSUNTOS
+        if linhaN.obterdispositivo(MensagemRecebida)!="ENERGYCONTROL":
+            #SEPARANDO OS ASSUNTOS
 
-        #MEDICAO
-        if assunto=="medicao":
-            print("O Assundo é Medicao")
-            now = datetime.now()
-            dispositivo = linhaN.obterdispositivo(MensagemRecebida)
-            horario = str(now.minute) + ":" + str(now.second)
-            consumo = linhaN.obterargumentos(MensagemRecebida)
-            self.Banco.inserirMedicao(dispositivo,horario,consumo[0])
+            #MEDICAO
+            if assunto=="medicao":
+                print("O Assundo é Medicao")
+                now = datetime.now()
+                dispositivo = linhaN.obterdispositivo(MensagemRecebida)
+                horario = str(now.minute) + ":" + str(now.second)
+                consumo = linhaN.obterargumentos(MensagemRecebida)
+                self.Banco.inserirMedicao(dispositivo,horario,consumo[0])
+
+            #Comando para o dispositivo
+            elif assunto=="comando_dispositivo":
+                print("O Assunto é um comando")
+                argumentos = linhaN.obterargumentos(MensagemRecebida)
+                dispositivo = argumentos[0]
+                comando = argumentos[1]
+#                mensagem = "ENERGYCONTROL/"+dispositivo+"/"+comando+"/"
+                mensagem = "ESPTomada1"
+
+                self.enviarMensagem(mensagem)
+        else:
+                print("Mensagem propia rcebida")
+
+
 pass
--- TString.py
+++ TString.py
@@ -3,17 +3,31 @@ class TString:
         x=10
 
     def obterassunto(self, linha):
-        linha0 = linha.split("'")
-        linha1 = linha0[1].replace("'","")
-        linhaFinal = linha1.split("/")
-        return linhaFinal[1]
+        if (linha.find("/") != -1):
+            if(linha.find("'")!= -1):
+                linha0 = linha.split("'")
+                linha1 = linha0[1].replace("'","")
+            else:
+                linha1 = linha
+
+            linhaFinal = linha1.split("/")
+            return linhaFinal[1]
+        else:
+            return ""
 
     def obterargumentos(self, linha):
-            linha0 = linha.split("'")
-            linha1 = linha0[1].replace("'","")
+        if(linha.find("/")!= -1):
+            if(linha.find("'")!=-1):
+                linha0 = linha.split("'")
+                linha1 = linha0[1].replace("'","")
+            else:
+                linha1 = linha
+
             linha2 = linha1.split("/")
             linhaFinal = linha2[2].split("%")
             return linhaFinal
+        else:
+            return ""
 
     def numargumentos(self,linha):
         linha0 = linha.split("/")
